1d4e4dd6c09d5f31940a1ac9fe8163382733c2f6
Atul Singh <46633779+hipswan@users.noreply.github.com>
2020-09-30 19:46:14 +0530

    widget app

--- lib/feed_grid.dart
+++ lib/feed_grid.dart
@@ -11,7 +11,7 @@ class FeedGrid extends StatelessWidget {
         crossAxisCount: 3,
         scrollDirection: Axis.vertical,
         children: feed
-            .map(
+            .map<Widget>(
               (e) => Image.network(
                   'https://www.pinkvilla.com/${e['field_photo_image_section']}'),
             )
